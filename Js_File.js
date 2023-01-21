720766efb71787c65bed659d8d20595200ce969b
Santhosh G <gorantalasanthosh@gmail.com>
2023-01-21 11:23:16 +0530

    Meassage to Commit

--- Js_File.js
+++ Js_File.js
@@ -1,3 +1,9 @@
-$(document).ready(function() {
-      $('#loading').fadeOut(3000);
-});
+// $(document).ready(function() {
+//       $('#loading').fadeOut(3000);
+// });
+
+const FirstName = "Santhosh"
+const LastName = "Gorantala"
+
+let FullName = FirstName + " " + LastName 
+console.log(FullName)
